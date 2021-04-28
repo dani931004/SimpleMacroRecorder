40f844e9128d54fd380488ef0c951fe9efb8a157
root <cvetaiii28@gmail.com>
2021-04-28 19:14:04 +0300

    done

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import pyautogui as pa
-import time
+from time import sleep
 
 positions = []
+count = 0
+
+print('Starting after 3 sec...')
+print('3...')
+sleep(1)
+print('2...')
+sleep(1)
+print('1...')
+sleep(1)
 # Record mouse movements
 # Take mouse position
-print('take position')
-position = pa.position()
-print('Position is:',position)
-# Save mouse position
-positions.append(position)
-print('sleep 5 sec...')
-time.sleep(5)
+while count < 50:
+    sleep(0.08)
+    print(count)
+    count += 1
+    position = pa.position()
+    print('Position is:',position)
+    # Save mouse position
+    positions.append(position)
 
-# Play the recorded mouse movements
-print('moving mouse')
-pa.moveTo(position)
-# Move mouse to saved position 
+print('Waiting 3 sec...')
+print('3...')
+sleep(1)
+print('2...')
+sleep(1)
+print('1...')
+sleep(1)
+
+
+
+
+  
+# Getting length of list
+i = 0
+# Iterating using while loop
+while i < count:
+    # Play the recorded mouse movements
+    print('moving mouse...')
+    pa.moveTo(positions[i])
+    i += 1
+    # Move mouse to saved position 
+
