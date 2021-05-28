ffdba205af0f3df4445ce377fa77dbd816db927d
Dani <cvetaiii28@gmail.com>
2021-05-28 14:44:39 +0300

    done

--- test.py
+++ test.py
@@ -3,29 +3,7 @@ from time import sleep
 from pynput import keyboard
 import logging
 # logging.basicConfig(filename="symbols2.txt", filemode='w', level=logging.DEBUG, format='%(message)s')
-
-
-symbols = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
-')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
-'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
-'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
-'browserback', 'browserfavorites', 'browserforward', 'browserhome',
-'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
-'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
-'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
-'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
-'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
-'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
-'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
-'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
-'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
-'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
-'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
-'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
-'up']
+speed = input('Please enter the speed between clicks in sec...')
 print('Waiting 3 sec...')
 print('3...')
 sleep(1)
@@ -34,13 +12,10 @@ sleep(1)
 print('1...')
 sleep(1)
 print('Playing...')
-symbols2 = []
-with open('symbols.txt', 'r') as f:
+with open('symbols2.txt', 'r') as f:
     a = f.readlines()
-    for line in a:
-        l = line.split(',')
-        if 'YES' in l[1] or 'ok' in l[1] or 'k' in l[1]:
-            symbols2.append(l[0])
-            print(l[0],l[1])
-            logging.info(l[0])
-    
+    for symbol in a:
+        symbol = symbol.replace('\n','')
+        pa.press(symbol)
+        print(symbol)
+        sleep(float(speed))
