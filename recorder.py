f9a2677e492b218b3e34904ddfe92a5b59c1ab31
Dani <cvetaiii28@gmail.com>
2022-01-11 16:28:25 +0200

    done

--- recorder.py
+++ recorder.py
@@ -3,6 +3,7 @@ from pynput.keyboard import Listener as key_listener
 from pynput import keyboard
 import logging
 import time
+import readline
 
 def play_recorder():
     global start
@@ -15,11 +16,24 @@ def play_recorder():
     # format='%(asctime)s: %(message)s'
     # --> to see when the key is pressed or clicked or scrolled
 
+    def on_move(x, y):
+        global start
+        stopp = time.time() - start
+        logging.info(f'move,{x},{y},{stopp}')
+        # print(f'move,{x},{y}')
+        start = time.time()
+        
     def on_click(x, y, button, pressed):
         global start
         if pressed:
             stopp = time.time() - start
             logging.info('click,{0},{1},{2},{3}'.format(x, y, button, stopp))
+            print('click,{0},{1},{2},{3}'.format(x, y, button, stopp))
+            start = time.time()
+        else:
+            stopp = time.time() - start
+            logging.info('release,{0},{1},{2},{3}'.format(x, y, button, stopp))
+            print('release,{0},{1},{2},{3}'.format(x, y, button, stopp))
             start = time.time()
 
 
@@ -27,6 +41,7 @@ def play_recorder():
         global start
         stopp = time.time() - start
         logging.info('scroll,{0},{1},{2},{3},{4}'.format(x, y, dx, dy, stopp))
+        # print('scroll,{0},{1},{2},{3},{4}'.format(x, y, dx, dy, stopp))
         start = time.time()
 
     def on_press(key):
@@ -35,17 +50,24 @@ def play_recorder():
             # Stopp listener
             mouse_listen.stop()
             keyboard_listen.stop()
+            print("\nRecorder has been stoped!\n")
             return False
         else:
             stopp = time.time() - start
             logging.info('press,{0},{1}'.format(key, stopp))
+            # print('press,{0},{1}'.format(key, stopp))
             start = time.time()
-    mouse_listen = mouse_listener(on_click=on_click, on_scroll=on_scroll)
     
+    keyboard_listen = key_listener(on_press=on_press)
+    keyboard_listen.start()
+    
+    with mouse_listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mouse_listen:
+        mouse_listen.join()
     # Setup the listener threads
-    keyboard_listen = key_listener(on_press=on_press) #, on_release=on_release
+    
+        #, on_release=on_release
 
     # Start the threads and join them so the script doesn't end early
-    keyboard_listen.start()
-    mouse_listen.start()
+    
+    
 
